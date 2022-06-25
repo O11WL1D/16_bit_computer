47ea7f0fbd8ff070b416c03bcbb3aa9cb61fe13d
O11WL1D <inksdomain@gmail.com>
2022-06-24 19:06:06 -0600

    default bash-save occured

--- Fpga implementation/16_bit/bus_split_out.vhd
+++ Fpga implementation/16_bit/bus_split_out.vhd
@@ -9,9 +9,9 @@ Entity bus_split_out is
 port(
 
 
-b : out std_logic_vector(15 downto 0) ;
+c : out std_logic_vector(15 downto 0) ;
 
-a0,a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12,a13,a14,a15 : in std_logic 
+d0,d1,d2,d3,d4,d5,d6,d7,d8,d9,d10,d11,d12,d13,d14,d15 : in std_logic 
 
 
  
@@ -31,22 +31,23 @@ end bus_split_out ;
 ARCHITECTURE LogicFunc of bus_split_out is
 Begin 
 
-b(0)<=	a0; 
-b(1)<=	a1; 
-b(2)<=	a2; 
-b(3)<=	a3; 
-b(4)<=	a4; 
-b(5)<=	a5; 
-b(6)<=	a6; 
-b(7)<=	a7; 
-b(8)<=	a8; 
-b(9)<=	a9; 
-b(10)<=	a10; 
-b(11)<=	a11; 
-b(12)<=	a12; 
-b(13)<=	a13; 
-b(14)<=	a14; 
-b(15)<=	a15; 
+
+c(0)<=	d0; 
+c(1)<=	d1; 
+c(2)<=	d2; 
+c(3)<=	d3; 
+c(4)<=	d4; 
+c(5)<=	d5; 
+c(6)<=	d6; 
+c(7)<=	d7; 
+c(8)<=	d8; 
+c(9)<=	d9; 
+c(10)<=	d10; 
+c(11)<=	d11; 
+c(12)<=	d12; 
+c(13)<=	d13; 
+c(14)<=	d14; 
+c(15)<=	d15; 
 
 
 
